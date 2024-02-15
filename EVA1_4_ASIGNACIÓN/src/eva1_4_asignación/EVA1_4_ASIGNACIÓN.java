6c3c3bd3b782fe80381e786e3e1b66be7d501fc6
LibeUwU <159199101+LibeUwU16@users.noreply.github.com>
2024-02-15 16:45:58 -0600

    Add files via upload

---
+++ EVA1_4_ASIGNACIÓN/src/eva1_4_asignación/EVA1_4_ASIGNACIÓN.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva1_4_asignación;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA1_4_ASIGNACIÓN {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       String nombre;
+       String carrera; 
+        int edad; //DECLARACIÓN
+        nombre = "Karina Yuleidy Sanchez Garfio";
+        carrera= "Ingieniería en sistemas computacionales";
+        edad = 19; //ASIGNACIÓN --> UN SOLO = SIGNIFICA ASIGNAR UN VALOR DENTRO DE LA VARIABLE
+        System.out.println("Tu nombre es");
+        System.out.println(nombre);
+        System.out.println("Tu carrera es");
+        System.out.println(carrera);
+        System.out.println("Tu edad es");
+        System.out.println(edad);
+    }
+    
+}
